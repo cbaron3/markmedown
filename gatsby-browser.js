1ce14b61ce16109ea1e6fa714852c48af3726f22
Carl Baron <carbaro196@gmail.com>
2020-09-18 23:07:49 -0400

    swap to simpler create-react-app

--- gatsby-browser.js
+++
@@ -1,2 +0,0 @@
-// Special file that gatsby looks for and uses if they exist
-require("./src/styles/global.css")
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
