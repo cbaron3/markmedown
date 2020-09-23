ce352f90ffbd06e33a8291ca506fa454c305655b
Carl Baron <carbaro196@gmail.com>
2020-09-23 00:50:04 -0400

    added text to settings icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,12 @@
+// TODO:
+//      1. Make title text responsive cause it dominates the header on small width screens
+//      2. Have settings icon color, text color,
+//          and markdown logo color (icon-black, icon-white) be configured for dark and light mode
+//      3. Settings menu on settings icon
+//      4. CSS clean up
+//      5. Create a global styles config for colors, fonts
+//      6. Remove magic numbers
+
 import React, { Component } from "react";
 
 import styled from "styled-components";
@@ -24,7 +33,6 @@ const StyledHeader = styled.header`
   grid-template: 50px / 1fr auto 1fr;
 `;
 
-// Move flex stuff into a div within the container
 const LogoContainer = styled.div`
   grid-column: 1 / 2;
   text-align: left;
@@ -54,6 +62,12 @@ const SettingsContainer = styled.div`
   grid-column: 3 / 4;
 `;
 
+const SettingsText = styled.span`
+  font-size: 18px;
+  font-weight: lighter;
+  padding-left: 5px;
+`;
+
 const LogoImg = styled.img`
   margin-right: 3px;
   padding-bottom: 3px;
@@ -63,8 +77,6 @@ const LogoText = styled.text`
   font-size: 26px;
 `;
 
-// HAVE LEARN TO BUILD README TEXT DISSAPPEAR AFTER CERTAIN WIDTH
-// OR REDUCE FONT SIZE?
 export default class Header extends Component {
   render() {
     return (
@@ -76,6 +88,7 @@ export default class Header extends Component {
         <TitleContainer>Learn to build your own README.md!</TitleContainer>
         <SettingsContainer>
           <UseAnimations animation={settings} size={32} strokeColor={"white"} />
+          <SettingsText>Settings</SettingsText>
         </SettingsContainer>
       </StyledHeader>
     );
